4bae9f5df15b18aa2fc20a645005a1bc3896d8c1
RechSuper <151687525+RechSuper@users.noreply.github.com>
2023-11-26 16:20:24 -0500

    mejorando el menú :)
    le agregue mas interacción y compatibilidad con diferentes pantallas

--- five nights with momos/Assets/Scripts/Managers/MenuManager.cs
+++ five nights with momos/Assets/Scripts/Managers/MenuManager.cs
@@ -3,10 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MenuManager : MonoBehaviour
-{// he creado un menu manager para que controle todo lo del menu principal: botones, animcaiones, efectos y etc
+{
+    public TextMeshProUGUI Noche;
+    int NumeroDeNoche;
+    // he creado un menu manager para que controle todo lo del menu principal: botones, animcaiones, efectos y etc
     public void NuevoJuego (){// este metodo es para el boton NuevoJuego
         SceneManager.LoadScene(1);//carga la escena de Noche 1 pues unity la toma como Scene 1.
+    }
+    public void Continuar(){//metodo para el boton de continuar
+        NumeroDeNoche++; // sumamos un no cada que es tocado el boton la noche
+        Noche.text = "night " + NumeroDeNoche; // luego esta es aplicada al texto
     }
 }
