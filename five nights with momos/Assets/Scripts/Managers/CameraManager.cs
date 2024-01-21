20e50503440ee6728ffa0672a1465e0c96302b2d
WolicraftSF <wolicraftsf@gmail.com>
2024-01-20 21:12:10 -0500

    idk

---
+++ five nights with momos/Assets/Scripts/Managers/CameraManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public int[] camerasID = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    public static string[] names = {"Show Stage", "OXXO Mini-Market", "Restrooms", "Playground", "Arcade Zone", "Kitchen", "Suply Room", "West Hall", "East Hall"};
+    public static Camera[] cameras;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
